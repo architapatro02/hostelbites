91b4d12a922c540d11b3a7c38c375be7584f87b3
HarshitaG03 <garg03harshita@gmail.com>
2024-03-09 21:32:59 +0530

    user home

--- lib/user/homeUser.dart
+++ lib/user/homeUser.dart
@@ -36,6 +36,335 @@ class _UserHomeState extends State<UserHome> {
         centerTitle: true,
         backgroundColor: Colors.brown[300],
       ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/bg9.jpg'), // Replace 'background_image.jpg' with your image path
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+                top: 185,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+                  height: MediaQuery.of(context).size.height - 230,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RichText(
+                            text: const TextSpan(
+                              text: "Today's Menu",
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Text(
+                            "Friday 8March",
+                            // style: GoogleFonts.montserrat(),
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w900,
+                                color:
+                                Colors.brown[900]
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.brown[100],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "8:00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.brown[900]),
+                                ),
+                                Text(
+                                  "AM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.brown[600]),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.brown.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width-120,
+                                  child: Text(
+                                    "  BREAKFAST",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                        width: MediaQuery.of(context).size.width-120,
+                                        //height: MediaQuery.of(context).size.height-80,
+                                        child: Text("  Milk,Chai,Toast,Bread,Butter,Jam",
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.brown[100],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "12:00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.brown[900]),
+                                ),
+                                Text(
+                                  "PM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.brown[600]),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.brown.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width-120,
+                                  child: Text(
+                                    "  LUNCH",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                        width: MediaQuery.of(context).size.width-120,
+                                        //height: MediaQuery.of(context).size.height-120,
+                                        child: Text("  Kadhi,Rice,Aalo gobhi,Chapati,Salad,Curd",
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.brown[100],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "5:00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.brown[900]),
+                                ),
+                                Text(
+                                  "PM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.brown[600]),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.brown.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width-120,
+                                  //height: MediaQuery.of(context).size.height-120,
+                                  child: Text(
+                                    " SNACKS",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                        width: MediaQuery.of(context).size.width-120,
+                                        // height: MediaQuery.of(context).size.height-160,
+                                        child: Text("  Chai,Fruits,Chips",
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.brown[100],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "8:00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.brown[900]),
+                                ),
+                                Text(
+                                  "PM",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                      color: Colors.brown[600]),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              height: 100,
+                              width: 1,
+                              color: Colors.brown.withOpacity(0.5),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  width: MediaQuery.of(context).size.width-120,
+                                  child: Text(
+                                    "  DINNER",
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                        width: MediaQuery.of(context).size.width-120,
+                                        //height: MediaQuery.of(context).size.height-120,
+                                        child: Text("  Rajma,Rice,Chapati,Salad",
+                                          overflow: TextOverflow.ellipsis,
+                                        )
+                                    )
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                )
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: signout,
         tooltip: 'Log OUT',
